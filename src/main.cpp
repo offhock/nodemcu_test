9db692c5f90dcddf6d2baccef03e997c5270e289
offhock <82597954+offhock@users.noreply.github.com>
2025-01-31 23:00:03 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ extern std::string ssid;
 extern std::string password;
 extern std::string mqtt_server;
 
-const char* nodeName = "nodemcu-1";
+const char* nodeName = "nodemcu-2";
 long lastSent = 0;
 
 WiFiClient wifiClient;
@@ -24,8 +24,11 @@ PubSubClient mqttClient(mqtt_server.c_str(), 1883, wifiClient);
 
 u16 count = 0;
 void setup() {    
-    Serial.begin(115200);    
+    Serial.begin(115200);
     Serial.printf("Serial is %d bps.\n", Serial.baudRate());
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH); // 스위치 OFF
 
     WiFi.begin(ssid.c_str(), password.c_str());
     Serial.print("Connecting to WiFi..");
@@ -39,12 +42,12 @@ void setup() {
         String message = String((char*)payload).substring(0, length) + '\0';        
         Serial.printf("Message arrived [%s], Payload: %s\n",topic, message.c_str());
         if (message.equals("ON")) {
-            digitalWrite(LED_BUILTIN, HIGH);  // 스위치 ON
+            digitalWrite(LED_BUILTIN, LOW);  // 스위치 ON
         } else if (message.equals("OFF")) {
-            digitalWrite(LED_BUILTIN, LOW);  // 스위치 OFF
+            digitalWrite(LED_BUILTIN, HIGH);  // 스위치 OFF
         }        
         // publish switch status
-        mqttClient.publish("homeassistant/nodemcu-1/switch", message.c_str());        
+        mqttClient.publish("homeassistant/nodemcu-2/switch", message.c_str());        
     });
 
 }
@@ -55,9 +58,9 @@ void reconnect() {
     if (mqttClient.connect(nodeName, "user", "passwd")) {
       Serial.println("connected");
       // subscribe switch setting
-      mqttClient.subscribe("homeassistant/nodemcu-1/switch/set"); 
-      // publish switch availability
-      mqttClient.publish("homeassistant/nodemcu-1/switch/available", "online"); 
+      mqttClient.subscribe("homeassistant/nodemcu-2/switch/set"); 
+      // publish switch available
+      mqttClient.publish("homeassistant/nodemcu-2/switch/available", "online");
     } else {
       Serial.print("failed, rc=");
       Serial.print(mqttClient.state());
@@ -70,8 +73,10 @@ void reconnect() {
 void loop() {
 
     if (!mqttClient.connected()) {
+        mqttClient.publish("homeassistant/nodemcu-2/switch/available", "offline"); 
         reconnect();
     } 
+
     mqttClient.loop();
 
     long now = millis();
@@ -79,12 +84,11 @@ void loop() {
         lastSent = now;        
         double value = 25 * sin(2.0 * PI * count / 100);
         double temperature = 25 + (count % 10);        
-        mqttClient.publish("homeassistant/nodemcu-1/temperature", String(temperature).c_str());
-        mqttClient.publish("homeassistant/nodemcu-1/counter", String(count).c_str());
-        mqttClient.publish("homeassistant/nodemcu-1/sin", String(value).c_str());
+        mqttClient.publish("homeassistant/nodemcu-2/temperature", String(temperature).c_str());
+        mqttClient.publish("homeassistant/nodemcu-2/counter", String(count).c_str());
+        mqttClient.publish("homeassistant/nodemcu-2/sin", String(value).c_str());
         Serial.printf("Temperature: %.1f, Counter: %d, Sin: %.2f\n", temperature, count, value);
         count++;
-    }
-    
+    }    
 }
  
